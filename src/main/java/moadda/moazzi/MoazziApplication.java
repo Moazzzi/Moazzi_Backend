85fb2b57d69b41bd9dcc70389e0410ee1755188b
yulhee741 <dbfgml980413@gmail.com>
2023-06-12 00:10:37 +0900

    [Setting] first commit

---
+++ src/main/java/moadda/moazzi/MoazziApplication.java
@@ -0,0 +1,13 @@
+package moadda.moazzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoazziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoazziApplication.class, args);
+	}
+
+}
---
+++ src/main/java/moadda/moazzi/domain/member/constant/SocialType.java
@@ -0,0 +1,9 @@
+package moadda.moazzi.domain.member.constant;
+
+import lombok.Getter;
+
+@Getter
+public enum SocialType {
+
+    GOOGLE, KAKAO, APPLE;
+}
