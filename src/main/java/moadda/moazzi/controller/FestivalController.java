86da6af4b6b875acdf38354bbf2beb8617da7c97
kim <vodjaos@gmail.com>
2023-06-18 22:20:10 +0900

    전국축제데이터 swagger 적용완료

--- src/main/java/moadda/moazzi/controller/FestivalController.java
+++ src/main/java/moadda/moazzi/controller/FestivalController.java
@@ -8,6 +8,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import moadda.moazzi.dto.festival.FestivalPageResponseDto;
 import moadda.moazzi.dto.festival.FestivalRequestDto;
@@ -18,6 +25,7 @@ import moadda.moazzi.service.OpenApiFestivalService;
 @RestController
 @RequestMapping("/api/v1/festivals")
 @RequiredArgsConstructor
+@Tag(name = "축제", description = "축제 정보 api 입니다.")
 public class FestivalController {
 	private final OpenApiFestivalService apiFestivalService;
 	private final FestivalService festivalService;
@@ -28,15 +36,25 @@ public class FestivalController {
 		return ResponseEntity.ok().build();
 	}
 	
+	@Operation(summary = "축제 데이터 리스트 조회", description = "축제 데이터 리스트 조회 메서드입니다.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(schema = @Schema(implementation = FestivalPageResponseDto.class))),
+        //@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = Error.class)))
+    })
 	@GetMapping
-	public ResponseEntity<?> getFestivals(FestivalRequestDto festivalRequestDto){
+	public ResponseEntity<FestivalPageResponseDto> getFestivals(@Parameter(description = "조회 조건") FestivalRequestDto festivalRequestDto){
 		System.out.println(festivalRequestDto);
 		FestivalPageResponseDto festivalPageResponseDto = festivalService.getFestivals(festivalRequestDto);
 		return ResponseEntity.ok(festivalPageResponseDto); 
 	}
 	
+	@Operation(summary = "축제 데이터 상세 조회", description = "축제 데이터 상세 조회 메서드입니다.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(schema = @Schema(implementation = FestivalResponseDto.class))),
+        //@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = Error.class)))
+    })
 	@GetMapping("/{festivalId}")
-	public ResponseEntity<?> getFestival(@PathVariable Long festivalId){
+	public ResponseEntity<FestivalResponseDto> getFestival(@Parameter(description = "축제데이터 ID") @PathVariable Long festivalId){
 		FestivalResponseDto festival = festivalService.getgetFestival(festivalId);
 		return ResponseEntity.ok(festival);
 	}
--- src/main/java/moadda/moazzi/dto/festival/FestivalPageResponseDto.java
+++ src/main/java/moadda/moazzi/dto/festival/FestivalPageResponseDto.java
@@ -2,14 +2,23 @@ package moadda.moazzi.dto.festival;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class FestivalPageResponseDto {
+	
+	@Schema(description = "페이지 번호", defaultValue = "1")
 	private int pageNo;
+	
+	@Schema(description = "페이지 당 데이터 수", defaultValue = "10")
 	private int numOfRows;
+	
+	@Schema(description = "데이터 총 갯수", defaultValue = "1000")
 	private Long totalCount;
+	
+	@Schema(description = "축제 데이터 리스트")
 	private List<FestivalResponseDto> data;
 }
