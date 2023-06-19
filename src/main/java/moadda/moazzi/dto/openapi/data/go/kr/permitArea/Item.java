7a3795cc017fa411cbfae871cf768ad2e925e508
kimtk22 <vodjaos@gmail.com>
2023-06-19 22:02:26 +0900

    푸드트럭허가구역 DTO 추가

---
+++ src/main/java/moadda/moazzi/dto/openapi/data/go/kr/permitArea/Item.java
@@ -0,0 +1,54 @@
+package moadda.moazzi.dto.openapi.data.go.kr.permitArea;
+
+import lombok.Data;
+
+@Data
+public class Item {
+    private String prmisnZoneNm;
+    private String lcType;
+    private String ctprvnNm;
+    private String signguNm;
+    private String rdnmadr;
+    private String lnmadr;
+    private String latitude;
+    private String longitude;
+    private String vhcleCo;
+    private String prmisnZoneRntfee;
+    private String beginDate;
+    private String endDate;
+    private String rstde;
+    private String weekdayOperOpenHhmm;
+    private String weekdayOperColseHhmm;
+    private String wkendOperOpenHhmm;
+    private String wkendOperCloseHhmm;
+    private String janOperOpenHhmm;
+    private String janOperColseHhmm;
+    private String febOperOpenHhmm;
+    private String febOperColseHhmm;
+    private String marOperOpenHhmm;
+    private String marOperColseHhmm;
+    private String aprOperOpenHhmm;
+    private String aprOperColseHhmm;
+    private String mayOperOpenHhmm;
+    private String mayOperColseHhmm;
+    private String junOperOpenHhmm;
+    private String junOperColseHhmm;
+    private String julOperOpenHhmm;
+    private String julOperColseHhmm;
+    private String augOperOpenHhmm;
+    private String augOperColseHhmm;
+    private String sepOperOpenHhmm;
+    private String sepOperColseHhmm;
+    private String octOperOpenHhmm;
+    private String octOperColseHhmm;
+    private String novOperOpenHhmm;
+    private String novOperColseHhmm;
+    private String decOperOpenHhmm;
+    private String decOperColseHhmm;
+    private String lmttPrdlst;
+    private String institutionNm;
+    private String phoneNumber;
+    private String referenceDate;
+    private String insttCode;
+
+}
